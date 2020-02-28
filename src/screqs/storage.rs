87ee5382f62be41a38773fb5bf3f3865c60290f6
Steve Roudebush <roudy@umich.edu>
2020-02-28 07:58:20 -0800

    get inserts working; had to get rid of multi insert :(

--- src/screqs/storage.rs
+++ src/screqs/storage.rs
@@ -1,5 +1,5 @@
 use crate::screqs::requests::TweetInfo;
-use rusqlite::{params, Connection, Result};
+use rusqlite::{params, Connection, Result, ToSql};
 use std::path::Path;
 
 pub fn create_conn() -> Result<Connection> {
@@ -42,41 +42,60 @@ pub fn create_tweet_table(conn: &Connection) -> std::result::Result<(), String> 
     return Ok(());
 }
 
-fn create_tweet_value_pack(twinfo: &TweetInfo) -> String {
-    return format!(
-        "({},{},{},'{}','{}','{}')",
-        twinfo.tweet_id(),
-        twinfo.user_id(),
-        twinfo.truncated() as u8,
-        twinfo.tweet_text(),
-        twinfo.user_name(),
-        twinfo.user_screen_name(),
-    );
-}
+fn create_prep_value_packs(num_packs: usize, num_params_per_pack: u32) -> String {
+    let mut sb = String::with_capacity(256);
 
-fn create_tweet_value_packs(twinfos: &Vec<TweetInfo>) -> String {
-    let packs: Vec<String> = twinfos
-        .into_iter()
-        .map(|twinfo| create_tweet_value_pack(twinfo))
-        .collect();
+    for j in 0..num_packs {
+        sb.push('(');
 
-    return packs.join(",");
+        for i in 1..(num_params_per_pack) {
+            sb.push('?');
+            sb.push_str(&i.to_string());
+            sb.push(',');
+        }
+
+        sb.push_str(&num_params_per_pack.to_string());
+        sb.push(')');
+
+        if j != (num_packs - 1) {
+            sb.push(',');
+        }
+    }
+
+    return sb;
 }
 
 pub fn insert_replace_tweets(
     conn: &Connection,
     twinfos: &Vec<TweetInfo>,
 ) -> std::result::Result<(), String> {
-    let packs = create_tweet_value_packs(twinfos);
-    let q = format!(
-        "insert or replace into mam_tweets
-        (tweet_id, user_id, truncated, tweet_text, user_name, user_screen_name) VALUES {};",
-        packs
-    );
+    let num_fields = 6;
+
+    // TODO: Batches
+    //let packs = create_prep_value_packs(twinfos.len(), num_fields);
+
+    let q = "insert or replace into mam_tweets
+        (tweet_id, user_id, truncated, tweet_text, user_name, user_screen_name) VALUES
+        (?1,?2,?3,?4,?5,?6);";
 
     println!("{}", q);
 
-    conn.execute(&q, params![]).unwrap();
+    for twinfo in twinfos {
+        let params = params!(
+            twinfo.tweet_id().to_string(),
+            twinfo.user_id().to_string(),
+            if twinfo.truncated() {
+                "1".to_string()
+            } else {
+                "0".to_string()
+            },
+            twinfo.tweet_text().to_string(),
+            twinfo.user_name().to_string(),
+            twinfo.user_screen_name().to_string(),
+        );
+
+        conn.execute(&q, params).unwrap();
+    }
 
     return Ok(());
 }
